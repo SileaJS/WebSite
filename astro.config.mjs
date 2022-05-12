7b16b5067dc4ae7190f321108b9e6ab8dbb01191
Nikola Hristov <nikola@playform.cloud>
2022-05-12 03:00:10 +0300

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import compress from "astro-compress";
 
 export default defineConfig({
 	site: "https://silea.dev",
+	experimental: {
+		integrations: true,
+	},
 	integrations: [
 		critters({
 			preload: "body",
