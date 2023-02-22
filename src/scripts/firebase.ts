5c89fb065df4a0ebf1ff13f00dee503677aa0f0f
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-22 05:06:22 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBb9d0h1NOUqhPcJKkomUpwvdXa7C6bO28",
-	appId: "1:427173895301:web:7850aa63dccb907e17ddec",
 	authDomain: "sileajs.firebaseapp.com",
 	databaseURL: "https://sileajs.firebaseio.com",
-	measurementId: "G-2KTSBJ1HZB",
-	messagingSenderId: "427173895301",
 	projectId: "sileajs",
 	storageBucket: "sileajs.appspot.com",
+	messagingSenderId: "427173895301",
+	appId: "1:427173895301:web:7850aa63dccb907e17ddec",
+	measurementId: "G-1SDRRZYV8C",
 };
 
 initializeApp(firebaseConfig);
