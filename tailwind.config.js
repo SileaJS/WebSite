5e33ca03b2c12e5a7bc982356f4565f9b9c8904c
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-11 01:20:05 +0300

    squash!

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		"./public/**/*.html",
-		"./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}",
+		"./Public/**/*.html",
+		"./Source/**/*.{astro,js,jsx,ts,tsx,vue,svelte}",
 	],
 	darkMode: "media",
 	theme: {
