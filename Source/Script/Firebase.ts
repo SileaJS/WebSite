3430048a8a94afa77311f78662f556f1079c663b
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-14 06:34:15 +0300

    squash!

--- Source/Script/Firebase.ts
+++ Source/Script/Firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 
 initializeApp({
-<<<<<<< HEAD
 	apiKey: "AIzaSyBb9d0h1NOUqhPcJKkomUpwvdXa7C6bO28",
 	authDomain: "sileajs.firebaseapp.com",
 	databaseURL: "https://sileajs.firebaseio.com",
@@ -10,14 +9,4 @@ initializeApp({
 	messagingSenderId: "427173895301",
 	appId: "1:427173895301:web:7850aa63dccb907e17ddec",
 	measurementId: "G-1SDRRZYV8C",
-=======
-	apiKey: "", // TODO Replace with your API key
-	appId: "", // TODO Replace with your app ID
-	authDomain: "", // TODO Replace with your auth domain
-	databaseURL: "", // TODO Replace with your database URL
-	measurementId: "", // TODO Replace with your measurement ID
-	messagingSenderId: "", // TODO Replace with your messaging sender ID
-	projectId: "", // TODO Replace with your project ID
-	storageBucket: "", // TODO Replace with your storage bucket
->>>>>>> upstream/main
 });
